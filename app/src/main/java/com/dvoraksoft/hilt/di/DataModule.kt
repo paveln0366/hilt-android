74868526def9caad0dd9e45f9ac3ac1c0e1d4977
Pavel Potapov <potapov@goozix.com>
2026-05-17 17:31:36 +0300

    Add Binds

--- app/src/main/java/com/dvoraksoft/hilt/di/DataModule.kt
+++ app/src/main/java/com/dvoraksoft/hilt/di/DataModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.dvoraksoft.hilt.data.Database
 import com.dvoraksoft.hilt.data.ExampleRepositoryImpl
 import com.dvoraksoft.hilt.domain.ExampleRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,21 +14,22 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataModule {
+interface DataModule {
 
     @Singleton
-    @Provides
-    fun provideExampleRepository(
+    @Binds
+    fun bindExampleRepository(
         impl: ExampleRepositoryImpl
-    ): ExampleRepository {
-        return impl
-    }
+    ): ExampleRepository
 
-    @Singleton
-    @Provides
-    fun provideDatabase(
-        @ApplicationContext context: Context
-    ): Database {
-        return Database.getInstance(context)
+    companion object {
+
+        @Singleton
+        @Provides
+        fun provideDatabase(
+            @ApplicationContext context: Context
+        ): Database {
+            return Database.getInstance(context)
+        }
     }
 }
